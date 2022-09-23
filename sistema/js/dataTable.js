5e059666386fa8930ff46347c79f31e33f00e6ab
Yuri Machado <ymachado1995@gmail.com>
2022-09-23 00:10:57 -0300

    sada

--- sistema/js/dataTable.js
+++ sistema/js/dataTable.js
@@ -1,18 +1,10 @@
-$(document).ready( function () {
-      
+
+
+$(document).ready(function () {
+
     var table = $('#orderTable').DataTable({
         paging: true,
-        searching: true,
-        language: {
-            lengthMenu: 'Exibir _MENU_ registros por página',
-            zeroRecords: 'Nada encontrado',
-            info: 'Mostrando página _PAGE_ de _PAGES_',
-            infoEmpty: 'Sem registros disponíveis',
-            infoFiltered: '(filtrados de _MAX_ total de registros)',
-            search: 'Pesquisa: ',
-            paginate: { previous: 'Anterior', next: 'Próxima' },
-        },
-    });
-
-
-} );
+        search: true,
+        serverside: true,
+    })
+})
